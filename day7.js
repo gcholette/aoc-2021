d2218da7eb8e70b70f8806fd7f3e6a7ad1843b98
gcholette <solutions@gcholette.com>
2021-12-07 00:38:49 -0500

    add day 7

---
+++ day7.js
@@ -0,0 +1,65 @@
+const { getChallengeData, runDay, sum, startChallenge, endChallenge } = require('./util')
+const day = 7
+
+const getMax = (positions) => positions.reduce((acc,curr) => curr > acc ? curr : acc, 0)
+const getMin = (positions) => positions.reduce((acc,curr,i) => curr < acc[0] ? [curr, i] : acc, [0, 0])
+
+function align(positions, variant = 1) {
+  const max = getMax(positions)
+  const solutions = []
+
+  if (variant === 2) {
+ for (let tries = 0; tries <= max; tries++) {
+    const newSolutions = positions.map(pos => {
+      const distance = Math.abs(pos - tries)
+      const wow = [...new Array(distance)].map((x, i) => i+1).reduce((acc,curr) => curr + acc, 0)
+      return wow
+    })
+    solutions.push(sum(newSolutions))
+  }
+  } else {
+for (let tries = 0; tries <= max; tries++) {
+    const newSolutions = positions.map(pos => {
+      return Math.abs(pos - tries)
+    })
+    solutions.push(sum(newSolutions))
+  }
+  }
+
+  
+
+  let min = null
+  for (let i = 0; i < solutions.length; i++) {
+    if (min === null) {
+      min = solutions[i]
+    } else if (solutions[i] < min) {
+      min = solutions[i]
+    }
+  }
+
+  return min
+}
+
+
+function challenge1() {
+  startChallenge(day, 1)
+  const array = getChallengeData(day, true)[0].split(',').map(x => +x)
+
+
+
+  const answer = align(array)
+  endChallenge(day, 1, answer)
+}
+
+function challenge2() {
+  startChallenge(day, 2)
+  const array = getChallengeData(day, false)[0].split(',').map(x => +x)
+
+
+
+  const answer = align(array, 2)
+  endChallenge(day, 2, answer)
+}
+
+runDay(day, challenge1, challenge2)
+
--- index.js
+++ index.js
@@ -25,5 +25,6 @@ require('./day1.js')
 require('./day2.js')
 require('./day3.js') 
 require('./day4.js') 
-require('./day5.js') */
-require('./day6.js')
+require('./day5.js') 
+require('./day6.js') */
+require('./day7.js')
